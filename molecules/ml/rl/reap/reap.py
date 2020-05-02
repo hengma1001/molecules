4124fbdec1774269c70881bdf14e836b87a25449
braceal <braceal@umich.edu>
2020-05-02 05:01:19 -0400

    q

--- molecules/ml/rl/reap/reap.py
+++ molecules/ml/rl/reap/reap.py
@@ -1,18 +1,33 @@
 import numpy as np
 from collections import namedtuple
 from itertools import starmap, repeat
+from molecules.ml.unsupervised.cluster import optics_clustering
 
+# TODO: use h5py
+#       https://stackoverflow.com/questions/25655588/incremental-writes-to-hdf5-with-h5py?rq=1
+#       https://stackoverflow.com/questions/47072859/how-to-append-data-to-one-specific-dataset-in-a-hdf5-file-with-h5py#47074545
 class State:
     def __init__(self):
         #State:    set of all discovered points (latent embeddings)
         #          on the landscape
-        self.state = np.array([])
+        self.data = np.array([])
 
     def save(self, fname):
-        np.save(self.state, fname)
+        np.save(self.data, fname)
 
-    def load(self):
-        pass
+    def load(self, fname):
+        self.data = np.load(fname)
+
+    def append(self, structures):
+        """
+        Effects
+        -------
+        Appends structures to data
+        
+        """
+        self.data = np.concatenate((self.data, structures))
+
+
 
 
 
@@ -24,17 +39,19 @@ class REAP:
                   on the landscape
     Action:       choosing protein structures to run more simulations on
     """
-    def __init__(self, num_order_params, least_sampling=5, prior_weights=None):
+    def __init__(self, num_order_params, least_samples=5, prior_weights=None):
 
         # CVAE latent dimension
         self.num_order_params = num_order_params
 
         # Number of clusters to examine when computing rewards
-        self.least_sampling = least_sampling
+        self.least_samples = least_samples
 
         # Same size as the number of order parameters. The ith weight
         # represents the importance of the ith order parameter.
         self.weights = self._init_weights(prior_weights)
+
+        self.state = State()
 
         Stats = namedtuple('Stats', ['mean', 'stdev'])
         self.stats = dict((op, Stats(0., 1.)) for op in self.order_parameters)
@@ -80,8 +97,9 @@ class REAP:
     def _optimize_weights(self):
         # Run maximization routine on the _cumulative_reward
         # See https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html#scipy.optimize.minimize
+        #  https://github.com/braceal/REAP-ReinforcementLearningBasedAdaptiveSampling/blob/master/MDSimulation/Src/RL/RLSim.py
 
-    def find_best_structures(self, structures):
+    def find_best_structures(self, structures, min_samples):
         """
         Chooses protein structures to spawn new simulations
         based on least poplated clusters and the reward
@@ -89,11 +107,21 @@ class REAP:
         """
 
         # 1. Add incomming structures to state
+        self.state.append(structures)
 
         # 2. Cluster the state into a set of L clusters using OPTICS
+        outlier_inds, labels = optics_clustering(self.state.data, min_samples)
 
         # 3. Identify subset of clusters which contain the least
         #    number of data points
+        uniques, counts = np.unique(labels)
+        # uniques: [-1, 1, 2, 3, 4, 5, ..., num_clusters]
+        # counts:  [5, 10, 11, 15, 17]
+        least_sampled = counts[np.argpartition(counts, kth=self.least_samples)][:self.least_samples + 1]
+
+        # argpartition sorts the kth element with [... k ...] the lower half of the array having all values smaller than k
+        # might not need to sort the k smallest values since we only need the associated clusters but not the size
+
 
         # 4. Compute reward for each structure in the subset of clusters
 
